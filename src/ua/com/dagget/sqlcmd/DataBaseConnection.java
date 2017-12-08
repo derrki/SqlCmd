dc20c4cb0d5c40499d690861835aae94b58ec66e
Roman_Kaminskiy <derrki@gmail.com>
2017-12-08 02:06:12 +0200

    3. В методі dBConnect () класу DataBaseConnection.java оптимізовано код для з'єднання з БД, а саме обєднано ClassNotFoundException та SQLException в один логічний блок. Перевірено роботу проекту, з'єднання з БД - коректне!.

--- src/ua/com/dagget/sqlcmd/DataBaseConnection.java
+++ src/ua/com/dagget/sqlcmd/DataBaseConnection.java
@@ -4,42 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DataBaseConnection {
+class DataBaseConnection {
 
-    public void dBConnect (){
+    void dBConnect() {
         try {
 
             Class.forName("org.postgresql.Driver");
 
-        } catch (ClassNotFoundException e) {
-
-            System.out.println("Where is your PostgreSQL JDBC Driver? "
-                    + "Include in your library path!");
-            e.printStackTrace();
-            return;
-
-        }
-
-        Connection connection = null;
-
-        try {
-
-            connection = DriverManager.getConnection(
+            Connection connection = DriverManager.getConnection(
                     "jdbc:postgresql://127.0.0.1:5432/sqlcmd", "postgres",
                     "postgres");
+            System.out.println("DB connected");
 
-        } catch (SQLException e) {
-
-            System.out.println("Connection Failed! Check output console");
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
-            return;
-
-        }
-
-        if (connection != null) {
-            System.out.println("You made it, take control your database now!");
-        } else {
-            System.out.println("Failed to make connection!");
         }
     }
 }
