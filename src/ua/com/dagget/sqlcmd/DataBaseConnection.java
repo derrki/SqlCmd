63beaa9b8e42478b736afa34d53d73d9bdd42dfd
Roman_Kaminskiy <derrki@gmail.com>
2017-12-15 22:59:47 +0200

    13. В клас  DataBaseConnection.java додано метод getConnection(), що повертає посилання на обєкт Connection. На даному етапі метод допомагає провести тест DataBaseConnectionTest.java шляхом перевірки  Connection != null. Дана версія тесту вже має якусь логіку роботи, павідміну від попередньої.

--- src/ua/com/dagget/sqlcmd/DataBaseConnection.java
+++ src/ua/com/dagget/sqlcmd/DataBaseConnection.java
@@ -4,34 +4,31 @@ import java.sql.*;
 
 class DataBaseConnection implements DataBaseHelper {
 
-    Connection connection = null;
-    Statement stmt = null;
+    private Connection connection = null;
+    private Statement stmt = null;
 
-    //Зєднання з базою
+    public Connection getConnection() {
+        return connection;
+    }
+
     @Override
     public void dBConnect() {
-
         //десь тут сховано роботу з JDBC !
         try {
-
-            //З'єднання з БД
-
             Class.forName(DataBaseContract.DRIVER);
-
             connection = DriverManager.getConnection(
                     DataBaseContract.HOST + DataBaseContract.DB_NAME, DataBaseContract.USERNAME,
                     DataBaseContract.PASSWORD);
-            System.out.println("DB connected");
 
         } catch (ClassNotFoundException | SQLException e) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
     }
 
     //Створення таблиці
     @Override
-    public void dBCreateTable(){
+    public void dBCreateTable() {
         try {
             stmt = connection.createStatement();
 
@@ -46,14 +43,14 @@ class DataBaseConnection implements DataBaseHelper {
 
             System.out.println("Table created");
         } catch (SQLException e) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
     }
 
     //Вставка даних в таблицю
     @Override
-    public void dBInsert(){
+    public void dBInsert() {
         try {
             connection.setAutoCommit(false);
             stmt = connection.createStatement();
@@ -76,7 +73,7 @@ class DataBaseConnection implements DataBaseHelper {
             stmt.close();
             connection.commit();
         } catch (SQLException e) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
 
@@ -84,28 +81,28 @@ class DataBaseConnection implements DataBaseHelper {
 
     //Вибірка даних з таблиці
     @Override
-    public void dBSelect(){
+    public void dBSelect() {
         try {
             connection.setAutoCommit(false);
             stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
-            while ( rs.next() ) {
+            ResultSet rs = stmt.executeQuery("SELECT * FROM COMPANY;");
+            while (rs.next()) {
                 int id = rs.getInt("id");
-                String  name = rs.getString("name");
-                int age  = rs.getInt("age");
-                String  address = rs.getString("address");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                String address = rs.getString("address");
                 float salary = rs.getFloat("salary");
-                System.out.println( "ID = " + id );
-                System.out.println( "NAME = " + name );
-                System.out.println( "AGE = " + age );
-                System.out.println( "ADDRESS = " + address );
-                System.out.println( "SALARY = " + salary );
+                System.out.println("ID = " + id);
+                System.out.println("NAME = " + name);
+                System.out.println("AGE = " + age);
+                System.out.println("ADDRESS = " + address);
+                System.out.println("SALARY = " + salary);
                 System.out.println();
             }
             rs.close();
             stmt.close();
         } catch (SQLException e) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
 
@@ -113,7 +110,7 @@ class DataBaseConnection implements DataBaseHelper {
 
     //Редагування даних в таблиці
     @Override
-    public void dBUpdate(){
+    public void dBUpdate() {
         try {
 
             stmt = connection.createStatement();
@@ -121,62 +118,62 @@ class DataBaseConnection implements DataBaseHelper {
             stmt.executeUpdate(sql);
             connection.commit();
 
-            ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+            ResultSet rs = stmt.executeQuery("SELECT * FROM COMPANY;");
 
-            while ( rs.next() ) {
+            while (rs.next()) {
                 int id = rs.getInt("id");
-                String  name = rs.getString("name");
-                int age  = rs.getInt("age");
-                String  address = rs.getString("address");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                String address = rs.getString("address");
                 float salary = rs.getFloat("salary");
 
-                System.out.println( "ID = " + id );
-                System.out.println( "NAME = " + name );
-                System.out.println( "AGE = " + age );
-                System.out.println( "ADDRESS = " + address );
-                System.out.println( "SALARY = " + salary );
+                System.out.println("ID = " + id);
+                System.out.println("NAME = " + name);
+                System.out.println("AGE = " + age);
+                System.out.println("ADDRESS = " + address);
+                System.out.println("SALARY = " + salary);
                 System.out.println();
             }
             rs.close();
             stmt.close();
             connection.close();
         } catch (SQLException e) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
     }
 
     //Видалення даних з таблиці
     @Override
-    public void dBDelete(){
+    public void dBDelete() {
         try {
 
             connection.setAutoCommit(false);
             stmt = connection.createStatement();
-            String sql = "DELETE from COMPANY where ID=4;";
+            String sql = "DELETE from COMPANY where ID=2;";
             stmt.executeUpdate(sql);
             connection.commit();
 
-            ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
+            ResultSet rs = stmt.executeQuery("SELECT * FROM COMPANY;");
 
-            while ( rs.next() ) {
+            while (rs.next()) {
                 int id = rs.getInt("id");
-                String  name = rs.getString("name");
-                int age  = rs.getInt("age");
-                String  address = rs.getString("address");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                String address = rs.getString("address");
                 float salary = rs.getFloat("salary");
 
-                System.out.println( "ID = " + id );
-                System.out.println( "NAME = " + name );
-                System.out.println( "AGE = " + age );
-                System.out.println( "ADDRESS = " + address );
-                System.out.println( "SALARY = " + salary );
+                System.out.println("ID = " + id);
+                System.out.println("NAME = " + name);
+                System.out.println("AGE = " + age);
+                System.out.println("ADDRESS = " + address);
+                System.out.println("SALARY = " + salary);
                 System.out.println();
             }
             rs.close();
             stmt.close();
         } catch (SQLException e) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
     }
--- test/ua/com/dagget/sqlcmd/DataBaseConnectionTest.java
+++ test/ua/com/dagget/sqlcmd/DataBaseConnectionTest.java
@@ -1,20 +1,26 @@
 package ua.com.dagget.sqlcmd;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import java.sql.Connection;
 
 public class DataBaseConnectionTest {
 
-    DataBaseConnection dataBaseConnection;
+   private DataBaseConnection dataBaseConnection;
 
     @Before
-    public void setup(){
+    public void setup() {
         dataBaseConnection = new DataBaseConnection();
+        dataBaseConnection.dBConnect();
     }
 
     @Test
-    public void testDataBaseConnection(){
-        dataBaseConnection.dBConnect();
+    public void testDataBaseConnection() {
+
+        Connection connectionActual = dataBaseConnection.getConnection();
+        Assert.assertNotNull(connectionActual);
+
     }
 
 }
